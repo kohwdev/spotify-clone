fa79450ef7b7a7da508b97045af191ac32519d2c
Woon Koh <woonsta13@gmail.com>
2023-10-04 16:57:35 -0700

    removed subscription

---
+++ app/account/components/AccountContent.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const AccountContent = () => {
+  const router = useRouter();
+
+  return <div></div>;
+};
+
+export default AccountContent;
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Figtree } from "next/font/google";
 import SupabaseProvider from "@/providers/SupabaseProvider";
 import UserProvider from "@/providers/UserProvider";
 import ModalProvider from "@/providers/ModalProvider";
-import ToasterProvider from "@/providers/ToasterProvier";
+import ToasterProvider from "@/providers/ToasterProvider";
 import getSongsByUserId from "@/actions/getSongsByUserId";
 import Player from "@/components/Player";
 
